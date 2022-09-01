da23a598870e663c68688a513ffb57af9716ce10
Brylan Gannon <brylan.gannon112@gmail.com>
2022-09-01 11:09:49 -0600

    Test: Model relationships and validations for Employee, Task, Assignments

--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Department, type: :model do
   describe 'relationships' do
     it { should have_many(:dept_managers) }
     it { should have_many(:managers).through(:dept_managers) }
+    it { should have_many(:employees) }
+    it { should have_many(:tasks) }
   end
 
   describe 'validations' do
