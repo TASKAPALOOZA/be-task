4a8e3354afe46bcf3de3b8e013f59bbb4aaa237a
Alex Psitos <apsitos08@gmail.com>
2022-09-08 14:02:30 -0400

    Test: add status checks to all sadpath tests

--- spec/requests/api/v1/manager_request_spec.rb
+++ spec/requests/api/v1/manager_request_spec.rb
@@ -34,8 +34,8 @@ RSpec.describe 'the manager request' do
 
     post '/api/v1/managers', headers: headers, params: JSON.generate(data) 
     
-    
     manager = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
     expect(manager).to have_key(:data)
     expect(manager[:data]).to have_key(:message)
     expect(manager[:data][:message]).to eq("All fields must be filled in")
@@ -52,10 +52,66 @@ RSpec.describe 'the manager request' do
 
     post '/api/v1/managers', headers: headers, params: JSON.generate(data) 
     
-    
     manager = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
     expect(manager).to have_key(:data)
     expect(manager[:data]).to have_key(:message)
     expect(manager[:data][:message]).to eq("All fields must be filled in")
+  end 
+
+  it 'returns an error message if password confirmation is blank' do 
+    data = {
+      "name": "Dudley Dooright",
+      "email": "dudley@testmail.com",
+      "password": "1234",
+      "password_confirmation": ""
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+    post '/api/v1/managers', headers: headers, params: JSON.generate(data) 
+    
+    manager = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
+    expect(manager).to have_key(:data)
+    expect(manager[:data]).to have_key(:message)
+    expect(manager[:data][:message]).to eq("All fields must be filled in")
+  end 
+
+  it 'returns an error message if email is already in use' do 
+    manager_1 = Manager.create!(name: "Bilbo", email: "bilbo@bilbo.bilbo", password: "1234", password_confirmation: "1234")
+
+    data = {
+      "name": "Steve",
+      "email": "bilbo@bilbo.bilbo",
+      "password": "1234",
+      "password_confirmation": "1234"
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+    post '/api/v1/managers', headers: headers, params: JSON.generate(data) 
+    
+    manager = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
+    expect(manager).to have_key(:data)
+    expect(manager[:data]).to have_key(:message)
+    expect(manager[:data][:message]).to eq("Email already in use")
+  end 
+
+  it 'returns an error message if password/password confirmation do not match' do 
+    data = {
+      "name": "Dude",
+      "email": "dude@dude.dude",
+      "password": "1234",
+      "password_confirmation": "4321"
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+    post '/api/v1/managers', headers: headers, params: JSON.generate(data) 
+    
+    manager = JSON.parse(response.body, symbolize_names: true)
+    expect(response.status).to eq(400)
+    expect(manager).to have_key(:data)
+    expect(manager[:data]).to have_key(:message)
+    expect(manager[:data][:message]).to eq("Passwords must match")
   end 
 end
