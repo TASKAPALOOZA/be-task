b4a85926c52c3f4482739319413247bb8c5e1d9b
Alex Psitos <apsitos08@gmail.com>
2022-09-08 13:27:49 -0400

    Feat: new and create actions added to Managers controller

---
+++ app/controllers/api/v1/managers_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::ManagersController < ApplicationController
+
+  def new 
+
+  end 
+
+  def create 
+    manager = Manager.new(manager_params)
+    binding.pry
+    if params[:name].nil? || params[:password].nil? || params[:password_confirmation].nil?
+      render json: SadpathSerializer.incomplete_fields, status: :bad_request
+    elsif Manager.find_by(email: manager.email)
+      render json: SadpathSerializer.email_in_use, status: :bad_request
+    elsif
+      params[:password] != params[:password_confirmation]
+      render json: SadpathSerializer.password_mismatch, status: :bad_request
+    elsif manager.save
+      render json: ManagerSerializer.new_manager_data(manager), status: 201
+    end
+  end 
+
+
+  private 
+    def manager_params
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
+end 
