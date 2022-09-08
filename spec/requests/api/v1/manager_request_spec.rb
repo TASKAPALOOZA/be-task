ee1ad796ce964ccb964291b3a09a0a8a25c4f4e2
Alex Psitos <apsitos08@gmail.com>
2022-09-08 13:26:40 -0400

    Fix: typo in manager create spec

--- spec/requests/api/v1/manager_request_spec.rb
+++ spec/requests/api/v1/manager_request_spec.rb
@@ -1,5 +1,3 @@
-
-
 require 'rails_helper'
 
 RSpec.describe 'the manager request' do
@@ -10,18 +8,18 @@ RSpec.describe 'the manager request' do
       "password": "1234",
       "password_confirmation": "1234"
     }
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
 
-    post '/api/v1/managers', params: data 
+    post '/api/v1/managers', headers: headers, params: JSON.generate(data) 
 
     manager = JSON.parse(response.body, symbolize_names: true)
-
     expect(response.status).to eq(201)
-    expect(user[:data]).to have_key(:type)
-    expect(user[:data]).to have_key(:id)
-    expect(user[:data]).to have_key(:attributes)
-    expect(user[:data[:attributes]]).to have_key(:name)
-    expect(user[:data[:attributes]]).to have_key(:email)
-    expect(user[:data[:attributes]]).to_not have_key(:password)
-    expect(user[:data[:attributes]]).to_not have_key(:password_confirmation)
+    expect(manager[:data]).to have_key(:type)
+    expect(manager[:data]).to have_key(:id)
+    expect(manager[:data]).to have_key(:attributes)
+    expect(manager[:data][:attributes]).to have_key(:name)
+    expect(manager[:data][:attributes]).to have_key(:email)
+    expect(manager[:data][:attributes]).to_not have_key(:password)
+    expect(manager[:data][:attributes]).to_not have_key(:password_confirmation)
   end
 end
