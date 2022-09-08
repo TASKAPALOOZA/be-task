3faccac545fc279d9f6c8bb33b520d2d0573b5b4
Alex Psitos <apsitos08@gmail.com>
2022-09-08 13:52:31 -0400

    Test: first two sadpath tests written and passed

--- spec/requests/api/v1/manager_request_spec.rb
+++ spec/requests/api/v1/manager_request_spec.rb
@@ -22,4 +22,40 @@ RSpec.describe 'the manager request' do
     expect(manager[:data][:attributes]).to_not have_key(:password)
     expect(manager[:data][:attributes]).to_not have_key(:password_confirmation)
   end
+
+  it 'returns an error message if name is blank' do 
+    data = {
+      "name": "",
+      "email": "test@testmail.com",
+      "password": "1234",
+      "password_confirmation": "1234"
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+    post '/api/v1/managers', headers: headers, params: JSON.generate(data) 
+    
+    
+    manager = JSON.parse(response.body, symbolize_names: true)
+    expect(manager).to have_key(:data)
+    expect(manager[:data]).to have_key(:message)
+    expect(manager[:data][:message]).to eq("All fields must be filled in")
+  end 
+
+  it 'returns an error message if password is blank' do 
+    data = {
+      "name": "Dudley Dooright",
+      "email": "dudley@testmail.com",
+      "password": "",
+      "password_confirmation": "1234"
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+
+    post '/api/v1/managers', headers: headers, params: JSON.generate(data) 
+    
+    
+    manager = JSON.parse(response.body, symbolize_names: true)
+    expect(manager).to have_key(:data)
+    expect(manager[:data]).to have_key(:message)
+    expect(manager[:data][:message]).to eq("All fields must be filled in")
+  end 
 end
